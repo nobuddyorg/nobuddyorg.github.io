598a1803645ac915a6f06279710b070492a2188c
Matthias Eggert <matthias.eggert@sogeti.com>
2025-07-22 15:53:24 +0200

    remove home, update header and footer for small screens

--- web-buddy/src/app/components/Footer.tsx
+++ web-buddy/src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import { SITE_URL, AUTHOR_NAME, GITHUB_URL } from "../globals";
 
 export default function Footer() {
   return (
-    <footer className="fixed bottom-0 left-0 w-full z-50 bg-white/80 dark:bg-black/80 backdrop-blur-sm border-t border-gray-200 dark:border-gray-800 py-6 text-center text-sm text-gray-400 dark:text-gray-600 px-4 sm:px-6">
+    <footer className="fixed bottom-0 left-0 w-full z-50 bg-white/80 dark:bg-black/80 backdrop-blur-sm border-t border-gray-200 dark:border-gray-800 py-3 md:py-6 text-center text-sm text-gray-400 dark:text-gray-600 px-4 sm:px-6">
       <p>
         Crafted with ♥️ by{" "}
         <a
--- web-buddy/src/app/components/Header.tsx
+++ web-buddy/src/app/components/Header.tsx
@@ -5,7 +5,7 @@ import { AUTHOR_NAME, GITHUB_URL } from "../globals";
 export default function Header() {
   return (
     <header className="fixed top-0 left-0 w-full z-50 bg-white/80 dark:bg-black/80 backdrop-blur-sm border-b border-gray-200 dark:border-gray-900">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 flex items-center justify-between h-16 overflow-hidden">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 flex items-center justify-between h-12 md:h-16 overflow-hidden">
         <Link
           href="/"
           className="text-2xl font-extrabold tracking-tight text-black dark:text-white"
@@ -21,13 +21,6 @@ export default function Header() {
           {AUTHOR_NAME}
         </Link>
         <div className="flex flex-wrap items-center space-x-4 sm:space-x-6">
-          <Link
-            href="/"
-            className="text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white font-semibold transition"
-            title="home"
-          >
-            Home
-          </Link>
           <Link
             href="/tools"
             className="text-gray-700 dark:text-gray-300 hover:text-black dark:hover:text-white font-semibold transition"
